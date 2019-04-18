4dadc0630e713b932299b01b5bcebec3aa48ad19
wangshuo <670681219@qq.com>
2019-04-18 15:47:54 +0800

    创建视图实现url跳转

--- pollsdemo/polls/views.py
+++ pollsdemo/polls/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse,HttpResponseRedirect
 # Create your views here.
+
+
+def Pollslist(request):
+    return HttpResponse("列表页")
+
+
