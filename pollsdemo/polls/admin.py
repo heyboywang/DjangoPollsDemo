74b690c708b5778fc0078eb2915fda515e5ccc44
wangshuo <670681219@qq.com>
2019-04-18 15:19:10 +0800

    创建站点和管理员 并自定义管理界面

--- pollsdemo/polls/admin.py
+++ pollsdemo/polls/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-# from .models import Options,Questions
+from .models import Options,Questions
 
 # Register your models here.
-# admin.site.register(Questions)
-# admin.site.register(Options)
+admin.site.register(Questions)
+
+class OptionsAdmin(admin.ModelAdmin):
+    list_display = ['id','option','num']
+
+admin.site.register(Options,OptionsAdmin)
--- pollsdemo/polls/models.py
+++ pollsdemo/polls/models.py
@@ -7,6 +7,15 @@ class Options(models.Model):
     votes = models.IntegerField(max_length=255)
     oQuestion = models.ForeignKey('Questions',on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.option
+    #更改后台列名显示
+    def num(self):
+        return self.votes
+    num.short_description = "票数"
+
 class Questions(models.Model):
     question = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.question
